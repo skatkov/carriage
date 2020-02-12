a6946de589c72ce03b9100d46e78d500c0ae9f40
Stanislav (Stas) Katkov <sk@skylup.com>
2020-02-12 17:42:11 +0400

    first version of carriage gem

--- lib/carriage.rb
+++ lib/carriage.rb
@@ -2,5 +2,25 @@ require "carriage/version"
 
 module Carriage
   class Error < StandardError; end
-  # Your code goes here...
+
+  BASE_URL = "https://www.amazon.com/gp/aws/cart/add.html?".freeze
+
+  def self.build(items, tag, key_id)
+    return nil if items.nil? || items.empty?
+
+    params = ""
+    index = 0
+
+    while index < items.size
+      params << '&' if index > 0
+      params << "ASIN.#{index+1}=#{items[index][:asin]}&Quantity.#{index+1}=#{items[index][:quantity]}"
+      index += 1
+    end
+
+    if key_id.nil? || key_id.empty?
+      "#{BASE_URL}#{params}&AssociateTag=#{tag}"
+    else
+      "#{BASE_URL}#{params}&AssociateTag=#{tag}&AWSAccessKeyId=#{key_id}"
+    end
+  end
 end
--- test/carriage_test.rb
+++ test/carriage_test.rb
@@ -5,7 +5,43 @@ class CarriageTest < Minitest::Test
     refute_nil ::Carriage::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  TAG = 'acart0c1-20'
+  KEY_ID = 'justkeyid'
+
+  def test_build_2
+    expected = append_keys(prepend_url("ASIN.1=B00WR23X5I&Quantity.1=1&ASIN.2=B018YJYPTA&Quantity.2=1"))
+    items = [{asin: 'B00WR23X5I', quantity: 1}, {asin: 'B018YJYPTA', quantity: 1}]
+
+    assert_equal expected, Carriage.build(items, TAG, KEY_ID)
+  end
+
+  def test_build_1
+    expected = append_keys(prepend_url("ASIN.1=B00WR23X5I&Quantity.1=1"))
+    items = [{asin: 'B00WR23X5I', quantity: 1}]
+    assert_equal expected, Carriage.build(items, TAG, KEY_ID)
+  end
+
+  def test_missing_key_id
+    expected = prepend_url("ASIN.1=B00WR23X5I&Quantity.1=1&AssociateTag=#{TAG}")
+    items = [{asin: 'B00WR23X5I', quantity: 1}]
+
+    assert_equal expected, Carriage.build(items, TAG, nil)
+    assert_equal expected, Carriage.build(items, TAG, "")
+  end
+
+  def test_zero
+    assert_nil Carriage.build({}, TAG, KEY_ID)
+    assert_nil Carriage.build(nil, TAG, KEY_ID)
+  end
+
+
+  private
+
+  def prepend_url(str)
+    "https://www.amazon.com/gp/aws/cart/add.html?#{str}"
+  end
+
+  def append_keys(str)
+    "#{str}&AssociateTag=#{TAG}&AWSAccessKeyId=#{KEY_ID}"
   end
 end
